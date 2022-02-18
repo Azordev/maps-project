162d54c05476052527b76ce88c293a22c96007e5
denisosuna <denisosuna@gmail.com>
2022-02-18 12:09:15 -0400

    fix: try cspeell

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React maro
         </a>
       </header>
     </div>
