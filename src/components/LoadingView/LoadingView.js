1075db4b0f9946a88d118112926b76f6e1db1886
Eghinner <eghinner@gmail.com>
2022-03-10 20:43:39 -0400

    feat: add twinkle animation

--- src/components/LoadingView/LoadingView.js
+++ src/components/LoadingView/LoadingView.js
@@ -2,12 +2,10 @@ import { Background, Icon } from './LoadingView.styled'
 import azordevIcon from '../../assets/azordev-icon.png'
 
 
-const LoadingView = () => {
-	return (
-		<Background>
-			<Icon src={azordevIcon}/>
-		</Background>
-	)
-}
+const LoadingView = () => (
+	<Background>
+		<Icon src={azordevIcon}/>
+	</Background>
+)
 
 export default LoadingView
--- src/components/LoadingView/LoadingView.styled.jsx
+++ src/components/LoadingView/LoadingView.styled.jsx
@@ -1,12 +1,24 @@
 import styled from '@emotion/styled'
+import { keyframes } from '@emotion/react'
 
+const twinkle = keyframes`
+  from,
+  0%,
+  to {
+    opacity: 0.5;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`
 export const Background = styled.div`
   width: 100vw;
   height: 100vh;
   position: fixed;
   top: 0;
   left: 0;
-  background: #9DBDFD;
+  background: var(--blueheader);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -14,6 +26,7 @@ export const Background = styled.div`
 export const Icon = styled.img`
   width: 300px;
   height: 188px;
+  animation: ${twinkle} 0.5s infinite alternate;
 
   @media (max-width: 900px) {
     width: 130px;
