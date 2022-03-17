0393f690c8d474a7f4226502ff987a0666407969
Eghinner <eghinner@gmail.com>
2022-03-17 01:51:35 -0400

    fix: styles Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import Layout from './Home.layout'
 
-const Home = () => <Layout title="Hola!" subtitle="Revisemos como viene tu envío" buttonText="ACEPTAR" />
+const Home = () => <Layout title="Hola!" subtitle="Revisemos como Viene tu envío" buttonText="ACEPTAR" />
 
 export default Home
